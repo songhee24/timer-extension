c4bb76c9b2caaa771617f13e1deaeecf789b62c7
songhee24 <akaratel61@gmail.com>
2024-01-04 09:58:19 +0600

    saving default notification

--- options.js
+++ options.js
@@ -1,14 +1,17 @@
 const nameInput = document.getElementById('name-input')
+const timeInput = document.getElementById('time-input')
+
 const saveBtn = document.getElementById('save-btn')
 
 saveBtn.addEventListener('click',() => {
-    const name = nameInput.value
-    chrome.storage.sync.set({ name: name }, () => {
+    const name = timeInput.value
+    const notificationTime = timeBtn.value
+    chrome.storage.sync.set({ name: name, notificationTime: notificationTime }, () => {
         console.log("Value is set");
     })
 })
 
-chrome.storage.sync.get(["name"], (result) => {
-    console.log("Value is " + JSON.stringify(result));
-    nameInput.value = result.name
+chrome.storage.sync.get(["name", "notificationTime"], (result) => {
+    nameInput.value = result.name ?? ''
+    timeInput.value = result.notificationTime ?? 1000
 });
