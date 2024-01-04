572066c01d875bde5ba86c92b5bdd9299581824d
songhee24 <akaratel61@gmail.com>
2024-01-04 08:43:19 +0600

    update logic of setting the value and also update logic to get a key

--- options.js
+++ options.js
@@ -2,9 +2,12 @@ const nameInput = document.getElementById('name-input')
 const saveBtn = document.getElementById('save-btn')
 
 saveBtn.addEventListener('click',() => {
-    const name = nameInput.name
+    const name = nameInput.value
     chrome.storage.sync.set({ name: name }, () => {
         console.log("Value is set");
     })
 })
 
+chrome.storage.sync.get(["name"], (result) => {
+    console.log("Value is " + JSON.stringify(result));
+});
